d8f2b612f4b978588a5db4605b1b959931fdde6c
joeco <joe@joeco.info>
2017-04-06 17:21:42 -0400

    refactor appsettings to proper angular injectable

--- src/app/AppSettings.ts
+++
@@ -1,6 +0,0 @@
-export let AppSettings = {
-  RAND_NUM_MAGNITUDE: 1000000000
-};
-/**
- * Created by joec on 3/4/2017.
- */
---
+++ src/app/app.settings.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class AppSettings {
+  RAND_NUM_MAXIMUM = 1000000000;
+  RAND_NUM_MINIMUM = 0;
+}
+
+/**
+ * Created by joec on 3/4/2017.
+ */
