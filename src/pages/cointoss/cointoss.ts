62923996c49a27c24717c8159dc4bb08e6b85104
joeco <joe@joeco.info>
2017-04-06 17:25:40 -0400

    Adds toin coss component

---
+++ src/pages/cointoss/cointoss.ts
@@ -0,0 +1,62 @@
+import { Component } from '@angular/core';
+import { HomePage } from '../../pages/home/home.ts';
+import { NavController } from 'ionic-angular';
+import { StoredNumbers } from "../../providers/storednums/storednums";
+import { shuffle } from 'lodash';
+
+@Component({
+  selector: 'page-cointoss',
+  templateUrl: 'cointoss.html'
+})
+export class CoinToss {
+  simulateFetching: boolean = false;
+  tossReqFulfilled: boolean = false;
+  heads: number = 0;
+  tails: number = 0;
+  result: string = '';
+  seedDataLength: number = 7;
+
+  constructor(
+    public navCtrl: NavController,
+    private storedNumbers: StoredNumbers
+  ) {
+    if (storedNumbers.storedNumArray.length < this.seedDataLength) {
+      storedNumbers.populateStoredNumbers();
+    }
+  }
+
+  toss(e: any) {
+    e && (e.preventDefault) && (e.preventDefault());
+    this.simulateFetching = true;
+    this.tossReqFulfilled = false;
+    const setResult = x => {
+      this.tossReqFulfilled = true;
+      if (x) {
+        return this.result = "heads";
+      }
+      this.result = "tails";
+    };
+    if (this.storedNumbers.storedNumArray.length < this.seedDataLength) {
+      return this.storedNumbers.populateStoredNumbers()
+        .then(() => setResult(this.calculateCoinToss()))
+        .catch(err => {});
+    }
+    setTimeout(() => {
+      setResult(this.calculateCoinToss());
+    }, 1500);
+  }
+
+
+  calculateCoinToss() {
+    this.simulateFetching = false;
+    const seed = Number('.' + shuffle(this.storedNumbers.storedNumArray.splice(0, this.seedDataLength)).join(''));
+    return Math.floor(seed * 2) == 0;
+  }
+
+
+  toPage(choice: number) {
+    this.navCtrl.push(
+      choice === 0 && (HomePage)
+    );
+  }
+}
